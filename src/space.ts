210c564a3a13fb3859db38380ad8c303ece17004
polyglot-chameleon <oleg.harlamov@web.de>
2025-12-25 17:51:27 +0100

    fix star size

--- src/space.ts
+++ src/space.ts
@@ -9,15 +9,9 @@ type Star = {
   y: number;
 };
 
-export default function initSpace(): void {
-  const { height, width, ctx }: Context = createCtx();
-
-  let stars = createStars(height, width);
-
-  window.addEventListener("resize", () => {
-    stars = createStars(height, width);
-  });
-
+export default function initSpace({ height, width, ctx }: Context): void {
+  ctx.clearRect(0, 0, width, height);
+  const stars = createStars(height, width);
   animate({ height, width, ctx, stars });
 }
 
@@ -26,25 +20,6 @@ const createStars = (height: number, width: number): Star[] =>
     x: Math.random() * width,
     y: Math.random() * height,
   }));
-
-function createCtx(): Context {
-  const canvas = document.createElement("canvas");
-  canvas.oncontextmenu = (e) => e.preventDefault();
-  canvas.style.position = "fixed";
-  canvas.style.top = "0";
-  canvas.style.left = "0";
-  canvas.style.width = "100%";
-  canvas.style.height = "100%";
-  canvas.style.zIndex = "-1";
-  document.body.appendChild(canvas);
-
-  const ctx = canvas.getContext("2d")!;
-  let width = window.innerWidth;
-  let height = window.innerHeight;
-  canvas.width = width;
-  canvas.height = height;
-  return { height, width, ctx };
-}
 
 function animate({
   width,
@@ -55,7 +30,7 @@ function animate({
   ctx.clearRect(0, 0, width, height);
 
   const speed = 0.05;
-  const size = 0.9 * (width / height);
+  const size = 1.3;
 
   stars.forEach((star) => {
     star.y -= speed;
