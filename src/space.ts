8465a57399d77423e3a3d241fe9317f9c236dcbe
polyglot-chameleon <61985465+polyglot-chameleon@users.noreply.github.com>
2025-12-25 16:24:18 +0100

    selective reload on resize (#14)

--- src/space.ts
+++ src/space.ts
@@ -1,7 +1,35 @@
-export default function initSpace(options: {
-  stars: { size: number; speed: number; color: string };
-}): void {
+type Context = {
+  height: number;
+  width: number;
+  ctx: CanvasRenderingContext2D;
+};
+
+type Star = {
+  x: number;
+  y: number;
+};
+
+export default function initSpace(): void {
+  const { height, width, ctx }: Context = createCtx();
+
+  let stars = createStars(height, width);
+
+  window.addEventListener("resize", () => {
+    stars = createStars(height, width);
+  });
+
+  animate({ height, width, ctx, stars });
+}
+
+const createStars = (height: number, width: number): Star[] =>
+  Array.from({ length: (height + width) / 4 }, () => ({
+    x: Math.random() * width,
+    y: Math.random() * height,
+  }));
+
+function createCtx(): Context {
   const canvas = document.createElement("canvas");
+  canvas.oncontextmenu = (e) => e.preventDefault();
   canvas.style.position = "fixed";
   canvas.style.top = "0";
   canvas.style.left = "0";
@@ -15,34 +43,31 @@ export default function initSpace(options: {
   let height = window.innerHeight;
   canvas.width = width;
   canvas.height = height;
+  return { height, width, ctx };
+}
 
-  const stars = Array.from({ length: (height + width) / 4 }, () => ({
-    x: Math.random() * width,
-    y: Math.random() * height,
-    size: options.stars.size,
-    speed: options.stars.speed,
-    color: options.stars.color,
-  }));
+function animate({
+  width,
+  height,
+  ctx,
+  stars,
+}: Context & { stars: Star[] }): void {
+  ctx.clearRect(0, 0, width, height);
 
-  function animate() {
-    ctx.clearRect(0, 0, width, height);
+  const speed = 0.05;
+  const size = 0.9 * (width / height);
 
-    stars.forEach((star) => {
-      star.y -= star.speed;
-      star.x += star.speed;
-      if (star.y < 0) star.y = height;
-      if (star.x > width) star.x = 0;
+  stars.forEach((star) => {
+    star.y -= speed;
+    star.x += speed;
+    if (star.y < 0) star.y = height;
+    if (star.x > width) star.x = 0;
 
-      ctx.fillStyle = star.color;
-      ctx.beginPath();
-      ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
-      ctx.fill();
-    });
+    ctx.fillStyle = "#888888";
+    ctx.beginPath();
+    ctx.arc(star.x, star.y, size, 0, Math.PI * 2);
+    ctx.fill();
+  });
 
-    requestAnimationFrame(animate);
-  }
-
-  window.addEventListener("resize", () => window.location.reload());
-
-  animate();
+  requestAnimationFrame(() => animate({ width, height, ctx, stars }));
 }
